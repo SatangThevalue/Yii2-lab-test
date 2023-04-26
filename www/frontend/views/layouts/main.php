d847fa9e0c089e2f89f68e534e81dd129b2b6d09
THANAPHOL NANTAKASET(Sa-Tang) <thanaphol369@gmail.com>
2023-04-26 11:13:28 +0700

    add Back End menu

--- www/frontend/views/layouts/main.php
+++ www/frontend/views/layouts/main.php
@@ -35,6 +35,7 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
+        ['label' => 'Back End', 'url' => ['/site/index']],
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
