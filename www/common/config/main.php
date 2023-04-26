fd4179c758e47b637c2d08f04b3f96b9c4f02609
THANAPHOL NANTAKASET(Sa-Tang) <thanaphol369@gmail.com>
2023-04-26 10:41:12 +0700

    Enable Pretty URL and set app in common

--- www/common/config/main.php
+++ www/common/config/main.php
@@ -1,11 +1,27 @@
 <?php
 return [
+    'charset' => 'UTF-8',
+    'language'=> 'th-TH',
+    'sourceLanguage'=> 'th-TH',
+    'timeZone' => 'Asia/Bangkok',
+    'version' => '0.0.1(Dev)',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        // TODO(SaTangTheValue): Enable Pretty URL
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+            ),
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
--- www/frontend/config/main.php
+++ www/frontend/config/main.php
@@ -36,17 +36,7 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        // TODO(SaTangTheValue): Enable Pretty URL
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => array(
-                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
-            ),
-        ],
+        
     ],
     'params' => $params,
 ];
